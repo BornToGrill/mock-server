971db9baab38e1b55ba14c9b42c68f3e69ff0383
Daniel <daniel@molenaar.dev>
2021-02-24 19:09:13 +0100

    Add logging

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
@@ -12,71 +14,96 @@ import (
 var (
 	port        int
 	status      int
+	contentType string
 	delay       int
 	body        string
-	contentType string
+
+	infoLog  = log.New(os.Stdout, "[INFO] ", log.LstdFlags)
+	errorLog = log.New(os.Stderr, "[ERROR] ", log.LstdFlags)
 )
 
 func main() {
 
 	flag.IntVar(&port, "port", 8080, "Port of the server")
 	flag.IntVar(&status, "status", 200, "Status code returned")
+	flag.StringVar(&contentType, "content-type", "application/json", "Content type of the response")
 	flag.IntVar(&delay, "delay", 0, "Amount of delay in response time")
 	flag.StringVar(&body, "body", "", "Body returned")
-	flag.StringVar(&contentType, "content-type", "application/json", "Content type of the response")
 
 	flag.Parse()
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		respStatus := getStatus(r)
-		respBody := getBody(r)
-		respContentType := getContentType(r)
-		delay := getDelay(r)
+	if stdin, ok := readStdinWhenAvailable(); ok {
+		body = stdin
+	}
 
-		// Artificial response time
-		time.Sleep(time.Duration(delay) * time.Millisecond)
-
-		rw.Header().Add("content-type", respContentType)
-		rw.WriteHeader(respStatus)
-		rw.Write([]byte(respBody))
-	})
+	http.HandleFunc("/", mockServerEndpoint)
 
 	listenAddr := fmt.Sprintf(":%d", port)
-	fmt.Printf("Server listening on %s\n", listenAddr)
+	infoLog.Print("Server listening on ", listenAddr)
 
-	log.Fatal(http.ListenAndServe(listenAddr, nil))
+	errorLog.Fatal(http.ListenAndServe(listenAddr, nil))
 }
 
-func getStatus(r *http.Request) int {
-	override := r.URL.Query().Get("status")
-	if override == "" {
-		return status
-	}
-	status, _ := strconv.Atoi(override)
-	return status
+func mockServerEndpoint(rw http.ResponseWriter, r *http.Request) {
+	respStatus := getIntOrDefault(r, "status", status)
+	respContentType := getStringOrDefault(r, "contentType", contentType)
+	respDelay := getIntOrDefault(r, "delay", delay)
+	respBody := getStringOrDefault(r, "body", body)
+
+	infoLog.Printf(`%s
+	Status: %d
+	ContentType: %s
+	Delay: %d
+	Body:
+%s`,
+		r.URL.RequestURI(),
+		respStatus,
+		respContentType,
+		respDelay,
+		respBody,
+	)
+
+	// Artificial response time
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+
+	rw.Header().Add("content-type", respContentType)
+	rw.WriteHeader(respStatus)
+	rw.Write([]byte(respBody))
 }
 
-func getDelay(r *http.Request) int {
-	override := r.URL.Query().Get("delay")
+func getStringOrDefault(r *http.Request, key, fallback string) string {
+	override := r.URL.Query().Get(key)
 	if override == "" {
-		return delay
+		return fallback
 	}
-	d, _ := strconv.Atoi(override)
+	return override
+}
+
+func getIntOrDefault(r *http.Request, key string, fallback int) int {
+	override := r.URL.Query().Get(key)
+	if override == "" {
+		return fallback
+	}
+	d, err := strconv.Atoi(override)
+	if err != nil {
+		errorLog.Printf("Failed to convert URL parameter `%s` returning default. Error: %v", key, err)
+		return fallback
+	}
 	return d
 }
 
-func getContentType(r *http.Request) string {
-	override := r.URL.Query().Get("contentType")
-	if override == "" {
-		return contentType
+func readStdinWhenAvailable() (string, bool) {
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		// Stdin available
+		infoLog.Print("YES")
+		b, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		return string(b), true
 	}
-	return override
-}
-
-func getBody(r *http.Request) string {
-	override := r.URL.Query().Get("body")
-	if override == "" {
-		return body
-	}
-	return override
+	// Stdin empty
+	infoLog.Print("NO")
+	return "", false
 }
